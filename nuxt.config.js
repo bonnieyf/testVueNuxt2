31fa06ca428d3919fd590694be3d66b04ab9e02e
kusasen <kusasen@gmail.com>
2020-04-23 10:40:08 +0800

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,7 +45,7 @@ module.exports = {
   */
   modules: [
     // Doc: https://github.com/nuxt-community/modules/tree/master/packages/bulma
-    '@nuxtjs/bulma',
+    // '@nuxtjs/bulma',
     '@nuxtjs/axios',
     '@nuxtjs/proxy',
     '@nuxtjs/style-resources',
--- pages/_lang/product.vue
+++ pages/_lang/product.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="center">
-    <div class="products grid-3_xs-1">
+    <!-- <div class="products grid-3_xs-1">
       <div class="col" v-for="(post,idx) in posts" :key="idx">
         <nuxt-link :to="{ name: 'lang-type-id' ,params: {id: post.id ,lang: $i18n.locale} }">
           <img src="https://picsum.photos/seed/picsum/500/500" alt="">
@@ -11,18 +11,37 @@
         </nuxt-link>
 
       </div>
-    </div>
+    </div> -->
+
+    <!-- {{datas}} -->
   </div>
 </template>
 
 <script>
 import axios from 'axios'
 export default {
-   asyncData({ req, params }) {
-    return axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then((res) => {
-          return { posts: res.data.slice(0, 5) }
-    })
-	}
+  // asyncData({ req, params }) {
+  // return axios.get('https://jsonplaceholder.typicode.com/posts')
+  //     .then((res) => {
+  //       return { posts: res.data.slice(0, 5) }
+  // })
+  // },
+  data(){
+    return{
+      datas: ''
+    }
+  },
+  // asyncData () {
+  //   return axios.get(`/api/CategoryFilter/skuCard`,{
+  //     headers: {
+  //       "X-Country-Id": 'gGFQgQDyENbap0wNCos4dw==',
+  //       "X-Site-Id": 'emu1p7U6zAP5WSXXoFWqfA=='
+  //     }
+  //   })
+  //     .then((res) => {
+  //       console.log(res)
+  //       // return {datas: res}
+  //     }).catch(error=>{console.log('error:'+error)})
+  // }
 };
 </script>
